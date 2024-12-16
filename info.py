a8df0912252f389619498556a1c78a97a8b7145a
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-12-16 17:59:52 +0530

    Add files via upload

--- info.py
+++ info.py
@@ -104,8 +104,8 @@ JOINREQ_MSG = bool(environ.get('JOINREQ_MSG', False))
 
 
 #first shortlink
-SHORTLINK_URL = environ.get('FIRST_SHORTLINK_URL', 'tryshort.in')
-SHORTLINK_API = environ.get('FIRST_SHORTLINK_API', '3058e5209596c0369b6ed7681b22f5e8216e02b5')
+SHORTLINK_URL = environ.get('FIRST_SHORTLINK_URL', 'shortxlinks.com')
+SHORTLINK_API = environ.get('FIRST_SHORTLINK_API', 'b474897e83e3e42619c67d2f56648aac5bb767ea')
 
 #second shortlink 
 SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'shortxlinks.com')
@@ -116,9 +116,8 @@ THIRD_SHORTLINK_URL = environ.get('THIRD_SHORTLINK_URL', 'tryshort.in')
 THIRD_SHORTLINK_API = environ.get('THIRD_SHORTLINK_API', '3058e5209596c0369b6ed7681b22f5e8216e02b5')
 
 
-
 #verify tutorial 
-VERIFY_TUTORIAL = environ.get('FIRST_VERIFY_TUTORIAL', 'https://t.me/how2dow/57')
+VERIFY_TUTORIAL = environ.get('FIRST_VERIFY_TUTORIAL', 'https://t.me/how2dow/76')
 SECOND_VERIFY_TUTORIAL = environ.get('SECOND_VERIFY_TUTORIAL', 'https://t.me/how2dow/76')
 THIRD_VERIFY_TUTORIAL = environ.get('THIRD_VERIFY_TUTORIAL', 'https://t.me/how2dow/57')
 
