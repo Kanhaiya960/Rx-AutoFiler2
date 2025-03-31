2ec495eb788bbe15dcbc7baa9d7b590052a2d67e
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-03-31 10:17:15 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -35,7 +35,7 @@ async def git(Kashmira, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powed by :</b> @spshah878
+<b>Powed by :</b> @TonyStark_Botz
 
 """
         if qw.get("description"):
