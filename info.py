9749033f6a7c395343e9b027c48df1ecf247ce3f
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-04-01 21:34:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ FORWARD_LINK = "https://vegamovies4u.xyz/wait?Autofiler2"
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS' ,'https://i.ibb.co/2YNN9MD2/photo-2025-03-31-08-17-12-7487884823235657744.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS' , 'https://i.ibb.co/2YNN9MD2/photo-2025-03-31-08-17-12-7487884823235657744.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://i.ibb.co/2YNN9MD2/photo-2025-03-31-08-17-12-7487884823235657744.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
